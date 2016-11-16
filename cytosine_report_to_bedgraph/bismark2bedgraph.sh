b35a81ccf3e91e4bda45c1d3a3ef7b0490d2899b
Charles BERNARD <chbernar@biocpc03.ens.fr>
2016-11-16 10:16:24 +0100

    modified bismark2bedgraph.sh

--- cytosine_report_to_bedgraph/bismark2bedgraph.sh
+++ cytosine_report_to_bedgraph/bismark2bedgraph.sh
@@ -8,27 +8,34 @@ eval set -- "$TEMP"
 while true ; do
 	case "$1" in
 		-e | --epi )
+			#epi is the prefix of the output files names
 			case "$2" in
 				"" ) epi="current_job"; shift 2 ;;
 				*) epi=$2; shift 2 ;;
 			esac ;;
 		-i | --infile_cov )
+			#infile_cov is the filename of the cytosine report taken as input
 			case "$2" in
 				*) infile_cov=$2; shift 2 ;;
 			esac ;;
 		-c | --context ) 
+			#context defines weither 2 or 4 bedgraphs are returned
 			context=true; shift ;;
 		--tdf )
+			#tdf defines weither or not bedgraphs have to be converted into tdf files.
 			tdf=true; shift ;;
 		--igv_genome )
+			#tdf conversion is achieved by igvtools and requires a file with the chrs_len of the genome
 			case "$2" in
 				*) igv_genome=$2; shift 2 ;;
 			esac ;;
 		-o | --output_dir )
+			#output_dir in this galaxy tool is the tmp dir created by the wrapper.py
 			case "$2" in
 				*) output_dir=$2; shift 2 ;;
 			esac ;;
 		--tool_dir )
+			#tool_dir is recquired to call other scripts stored in this directory
 			case "$2" in
 				*) tool_dir=$2; shift 2 ;;
 			esac ;;
@@ -43,16 +50,20 @@ done
 # define outputs according to options
 if [[ "$context" = true ]]; then
 	context_list=("CG" "CHG" "CHH")
+	n="4"
 	output_types=("CG" "CHG" "CHH" "coverage")
 	bedgraph_list=("$output_dir""/""$epi""_CpG.bedgraph" "$output_dir""/""$epi""_CHG.bedgraph" "$output_dir""/""$epi""_CHH.bedgraph" "$output_dir""/""$epi""_coverage.bedgraph")
-	tdf_list=("$output_dir""/""$epi""_CpG.tdf" "$output_dir""/""$epi""_CHG.tdf" "$output_dir""/""$epi""_CHH.tdf" "$output_dir""/""$epi""_coverage.tdf")
-	n="4"
+	if [[ "$tdf" = true ]]; then
+		tdf_list=("$output_dir""/""$epi""_CpG.tdf" "$output_dir""/""$epi""_CHG.tdf" "$output_dir""/""$epi""_CHH.tdf" "$output_dir""/""$epi""_coverage.tdf")
+	fi
 else
 	context_list=(".*")
+	n="2"
 	output_types=("CXX" "coverage")
 	bedgraph_list=("$output_dir""/""$epi""_CXX.bedgraph" "$output_dir""/""$epi""_coverage.bedgraph")
-	tdf_list=("$output_dir""/""$epi""_CXX.tdf" "$output_dir""/""$epi""_coverage.tdf")
-	n="2"
+	if [[ "$tdf" = true ]]; then
+		tdf_list=("$output_dir""/""$epi""_CXX.tdf" "$output_dir""/""$epi""_coverage.tdf")
+	fi
 fi
 
 # process
@@ -60,13 +71,10 @@ for (( i=0; i<$n; i++)); do
 	printf "________________________________________________________________________\n"
 	printf "Processing %s\n" ${output_types[$i]}
 	printf "... Converting Cytosine Report to Bedgraph\n" 
-	if (( i < n - 1 )); then
-		#if not coverage:
-		#printf "track type=bedGraph name=%s Coverage description=%s Coverage\n" "$epi""_""${context_list[$i]}" "$epi""_""${context_list[$i]}" > "${bedgraph_list[$i]}"
+	if (( i < n - 1 )); then #if not coverage:
 		printf "#<Chr>\t<Start>\t<End>\t<Strand;Meth_ratio>\n" > "${bedgraph_list[$i]}"
 		awk -v context="${context_list[$i]}" -v coverage="false" -f "$tool_dir"/bismark2bedgraph.awk $infile_cov >> "${bedgraph_list[$i]}"
 	else
-		#printf "track type=bedGraph name=%s Coverage description=%s Coverage\n" "$epi""_""${context_list[$i]}" "$epi""_""${context_list[$i]}" > "${bedgraph_list[$i]}"
 		printf "#<Chr>\t<Start>\t<End>\t<Coverage>\n" > "${bedgraph_list[$i]}"
 		awk -v context="${context_list[$i]}" -v coverage="true" -f "$tool_dir"/bismark2bedgraph.awk $infile_cov >> "${bedgraph_list[$i]}"
 	fi
